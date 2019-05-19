7823fc14189c44c21139cbc8cc25f9681959187b
Antonenko Bohdan <bohdan1997@gmail.com>
2019-05-19 11:16:10 +0300

    solution

---
+++ js.js
@@ -0,0 +1,26 @@
+'use strict';
+const ball = document.querySelector('.ball');
+const field = document.querySelector('.field');
+
+field.onclick = function (event) {
+    let top = event.clientY  - field.clientTop - ball.offsetHeight / 2;
+    let left = event.clientX - field.clientLeft - ball.offsetWidth / 2;
+    let maxLeft = field.clientWidth - ball.offsetWidth;
+    let maxTop = field.clientHeight - ball.offsetHeight;
+
+    if ( left > maxLeft) {
+        left = maxLeft;
+    }
+    if ( left < 0) {
+        left = 0;
+    }
+    if (top > maxTop) {
+        top = maxTop;
+    }
+    if ( top < 0) {
+        top = 0;
+    }
+
+    ball.style.left = `${left}px`
+    ball.style.top = `${top}px`
+}
